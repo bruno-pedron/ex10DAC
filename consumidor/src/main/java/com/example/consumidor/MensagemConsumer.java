5e4b2705b2963b99be0307558a5848f5650f3544
Bruno Pedron Rupaner <165921892+bruno-pedron@users.noreply.github.com>
2026-05-18 09:35:37 -0300

    Add RabbitMQ message consumer with listener

--- consumidor/src/main/java/com/example/consumidor/MensagemConsumer.java
+++ consumidor/src/main/java/com/example/consumidor/MensagemConsumer.java
@@ -1,1 +1,13 @@
+package com.exemplo.consumidor;
 
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MensagemConsumer {
+
+    @RabbitListener(queues = RabbitConfig.NOME_FILA)
+    public void consumirMensagem(String mensagem) {
+        System.out.println("Mensagem recebida: " + mensagem);
+    }
+}
